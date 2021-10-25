1b566c46af289028ec8f0d4a2eb63998be322b3a
Nicolas Burrus <nicolas@burrus.name>
2021-10-25 19:49:38 +0200

    Fix the generate unit test.

--- tests/test_generate.py
+++ tests/test_generate.py
@@ -13,19 +13,19 @@ from daltonlens import convert, simulate, generate
 class TestIshiharaPlate(unittest.TestCase):
 
     def test_protanopia(self):
-        im = generate.ishihara_plate(simulate.Deficiency.PROTAN, 1.0, "Protan Severity 1.0")
+        im = generate.ishihara_plate_dichromacy(simulate.Deficiency.PROTAN, "Protan Severity 1.0")
         Image.fromarray(im).save("plate_protan_1.0.png")
 
     def test_protanomaly(self):
-        im = generate.ishihara_plate(simulate.Deficiency.PROTAN, 0.5)
+        im = generate.ishihara_plate_dichromacy(simulate.Deficiency.PROTAN)
         Image.fromarray(im).save("plate_protan_0.5.png")
 
     def test_deuteranopia(self):
-        im = generate.ishihara_plate(simulate.Deficiency.DEUTAN, 1.0)
+        im = generate.ishihara_plate_dichromacy(simulate.Deficiency.DEUTAN)
         Image.fromarray(im).save("plate_deutan_1.0.png")
 
     def test_tritanopia(self):
-        im = generate.ishihara_plate(simulate.Deficiency.TRITAN, 1.0)
+        im = generate.ishihara_plate_dichromacy(simulate.Deficiency.TRITAN)
         Image.fromarray(im).save("plate_tritan_1.0.png")
 
 if __name__ == '__main__':
